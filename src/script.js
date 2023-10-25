6ed8b78fa01b180499c3823aabd6760fe5d7a7e4
lrglucas97 <lrglucas97@gmail.com>
2023-10-24 21:38:02 -0400

    Optimizing if...else structures.

--- src/script.js
+++ src/script.js
@@ -4,19 +4,19 @@ let level = ""
 
 if (xp < 1000) {
   level = "Iron";
-} else if (xp >= 1001 && xp <= 2000) {
+} else if (xp <= 2000) {
   level = "Bronze";
-} else if (xp >= 2001 && xp <= 6000) {
+} else if (xp <= 6000) {
   level = "Silver";
-} else if (xp >= 6001 && xp <= 7000) {
+} else if (xp <= 7000) {
   level = "Gold";
-} else if (xp >= 7001 && xp <= 8000) {
+} else if (xp <= 8000) {
   level = "Platinum";
-} else if (xp >= 8001 && xp <= 9000) {
+} else if (xp <= 9000) {
   level = "Ascending";
-} else if (xp >= 9001 && xp <= 10000) {
+} else if (xp <= 10000) {
   level = "Immortal";
-} else if (xp >= 10001) {
+} else {
   level = "Radiant";
 }
 
